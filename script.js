b20222a8dd96d7841ec5342fe922f64dc6bc94df
jpmunich <parkercoyle8@gmail.com>
2022-11-25 16:28:14 -0500

    Numbers appear on calculator when pressed

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const input = document.getElementById("input");
 
 
 for (let i = 0; i < calculatorButtons.length; i++) {
-    console.log(calculatorButtons[i].value);
     calculatorButtons[i].addEventListener("click", function() {
-        const text = document.createTextNode(calculatorButtons[i].getAttribute("value"));
-        input.innerText += text;
+                input.innerText += calculatorButtons[i].value;
         })
 }
