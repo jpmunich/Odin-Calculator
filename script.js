66e4ecf9796f8dad095a4ce679095fd1c640603e
jpmunich <parkercoyle8@gmail.com>
2022-12-19 18:44:30 -0500

    Added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Initialize variables
 const calculatorButtons = document.getElementsByClassName("calculator-button");
 const input = document.getElementById("input");
 const additionButton = document.getElementById("calculator-button-addition");
@@ -6,33 +7,36 @@ const clearCalculatorButton = document.getElementById("calculator-button-clear")
 
 let hasClickedEquals = false;
 
+// Iterates through each button to allow user to type
 for (let i = 0; i < calculatorButtons.length; i++) {
     calculatorButtons[i].addEventListener("click", function() {
                 input.innerText += calculatorButtons[i].value;
         })
 }
 
+// Clears calculator
 clearCalculatorButton.addEventListener("click", function() {
     input.innerText = "";
 })
 
 
-
+// Addition
 additionButton.addEventListener("click", function() {
     const addendOneInput = input.innerText;
     const addendOne = addendOneInput.substring(0, (addendOneInput.length - 1));
-    
+    console.log(addendOne);
     let addendTwo = "";
 
     equalsButton.addEventListener("click", function() {
         const addendTwoInput = input.innerText;
-        addendTwo += addendTwoInput.substring(addendOneInput.length, addendTwoInput.length);
+
+        addendTwo = addendTwoInput.substring(addendOneInput.length, addendTwoInput.length);
+        console.log(addendTwo);
 
         const addendOneNum = parseInt(addendOne);
         const addendTwoNum = parseInt(addendTwo);
 
-        const sum = addendOneNum + addendTwoNum;
+        let sum = addendOneNum + addendTwoNum;
         input.innerText = sum;
     })
-
 })
