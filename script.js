8ded8563bcb53a26679456b282436d8b301f852f
jpmunich <parkercoyle8@gmail.com>
2022-12-21 12:42:26 -0500

    Reworked code so that all operations are possible + Added comments to js

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 // Initialize variables
 const calculatorButtons = document.getElementsByClassName("calculator-button");
+const operatorButtons = document.getElementsByClassName("operator-button");
+
+let operator = null;
+
+const previousInput = document.getElementById("previous-input");
 const input = document.getElementById("input");
-const additionButton = document.getElementById("calculator-button-addition");
+
 const equalsButton = document.getElementById("calculator-button-equals");
 const clearCalculatorButton = document.getElementById("calculator-button-clear");
 
-let hasClickedEquals = false;
-
 // Iterates through each button to allow user to type
 for (let i = 0; i < calculatorButtons.length; i++) {
-    calculatorButtons[i].addEventListener("click", function() {
-                input.innerText += calculatorButtons[i].value;
-        })
+  calculatorButtons[i].addEventListener("click", function() {
+    input.innerText += calculatorButtons[i].value;
+  });
 }
+
+// Set the operator based on the operator button clicked
+for (let i = 0; i < operatorButtons.length; i++) {
+  operatorButtons[i].addEventListener("click", function() {
+    operator = operatorButtons[i].value;
+    previousInput.innerText = input.innerText;
+    input.innerText = "";
+  });
+}
+
+// Processes the input and evaluates the expression
+equalsButton.addEventListener("click", function() {
+  const firstInput = parseFloat(previousInput.innerText);
+  const secondInput = parseFloat(input.innerText);
+
+  if (operator === "+") {
+    input.innerText = firstInput + secondInput;
+  } else if (operator === "-") {
+    input.innerText = firstInput - secondInput;
+  } else if (operator === "*") {
+    input.innerText = firstInput * secondInput;
+  } else if (operator === "/") {
+    input.innerText = firstInput / secondInput;
+  }
+});
 
 // Clears calculator
 clearCalculatorButton.addEventListener("click", function() {
-    input.innerText = "";
-})
+  input.innerText = "";
+});
 
-
-// Addition
-additionButton.addEventListener("click", function() {
-    const addendOneInput = input.innerText;
-    const addendOne = addendOneInput.substring(0, (addendOneInput.length - 1));
-    console.log(addendOne);
-    let addendTwo = "";
-
-    equalsButton.addEventListener("click", function() {
-        const addendTwoInput = input.innerText;
-
-        addendTwo = addendTwoInput.substring(addendOneInput.length, addendTwoInput.length);
-        console.log(addendTwo);
-
-        const addendOneNum = parseInt(addendOne);
-        const addendTwoNum = parseInt(addendTwo);
-
-        let sum = addendOneNum + addendTwoNum;
-        input.innerText = sum;
-    })
-})
